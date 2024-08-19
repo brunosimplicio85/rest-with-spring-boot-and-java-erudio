fe2decca879d99f178989e915122024f5e751424
brunosimplicio85 <bruno.simplicio@outlook.com>
2024-08-19 13:26:53 -0300

    First Steps in Java With Spring Boot 3

---
+++ 05_FirstStepsInJavaSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/simplicio/demo/StartupTests.java
@@ -0,0 +1,13 @@
+package simplicio.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
